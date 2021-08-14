b9caa6b18cff787fffa190d5fe5d9730d8d45ee9
Asraful Islam <iasraful321@gmail.com>
2021-08-14 20:51:22 +0600

    array in intermidate

--- script.js
+++ script.js
@@ -10,5 +10,14 @@
 
 // console.log(printHi("Kayel"));
 
-const newArr = ["a", "b", "c", "d", "e"]
-console.log(newArr[2]);
+// const newArr = ["a", "b", "c", "d", "e"]
+// console.log(newArr[2]);
+
+const a = [
+[1, 2, 3, 4, 5],
+[6, 7, 8, 9, 10], 
+[11, 12, 13, 14, 15]  //opt 4, 8, 11
+]
+console.log(a[0][3]);
+console.log(a[1][2]);
+console.log(a[2][0]);
