300b6f082308159ee708355f055f40dd9f182ab1
Asraful Islam <iasraful321@gmail.com>
2021-08-14 20:30:32 +0600

    Array is done by useing []

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function any(variable) {
-    console.log(variable)
-}
-function result(name, callback) {
-    callback("Hello " + name);
-}
-result("Kayel", any)
+// function any(variable) {
+//     console.log(variable)
+// }
+// function result(name, callback) {
+//     callback("Hello " + name);
+// }
+// result("Kayel", any)
+
+// let printHi = name => "Hi " + name
+
+// console.log(printHi("Kayel"));
+
+const newArr = ["a", "b", "c", "d", "e"]
+console.log(newArr[2]);
