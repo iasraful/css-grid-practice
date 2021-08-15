e3bf9bca2b828dc71c7b9a754e99fb6116a4222e
Asraful Islam <iasraful321@gmail.com>
2021-08-15 17:34:39 +0600

    done for today

--- script.js
+++ script.js
@@ -13,11 +13,46 @@
 // const newArr = ["a", "b", "c", "d", "e"]
 // console.log(newArr[2]);
 
-const a = [
-[1, 2, 3, 4, 5],
-[6, 7, 8, 9, 10], 
-[11, 12, 13, 14, 15]  //opt 4, 8, 11
+// const a = [
+// [1, 2, 3, 4, 5],
+// [6, 7, 8, 9, 10], 
+// [11, 12, 13, 14, 15]  //opt 4, 8, 11
+// ]
+// console.log(a[0][3]);
+// console.log(a[1][2]);
+// console.log(a[2][0]);
+
+    // Simflied Class: 29 number lecture
+
+// const car = {
+//     make: "Made in Japan",
+//     model: "Honda Cevic Type-R",
+//     isUsed: false,
+//     makeNois(){
+//         console.log("Vroom");
+//     }
+// }
+// console.log(car);
+// car.makeNois()
+// const book = {
+//     titel: "A Thousend Years",
+//     Author: {
+//         name: "Asraful Islam",
+//         age: 23
+//     }
+// }
+// console.log(book.Author.name);\
+
+const items = [
+    {price:123},
+    {price:103},
+    {price:23},
+    {price:43},
+    {price:12}
 ]
-console.log(a[0][3]);
-console.log(a[1][2]);
-console.log(a[2][0]);
+
+const ami = items.reduce((sum, number) =>{
+    return sum + number.price           
+}, 0)
+
+console.log(ami);
