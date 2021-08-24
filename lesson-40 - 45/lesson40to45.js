7a0469ccdf6d5514a8e29650af0ed0eed5770000
Asraful Islam <iasraful321@gmail.com>
2021-08-25 00:31:20 +0600

    data attributes in jS, And Added 41 Practice task:

--- lesson-40 - 45/lesson40to45.js
+++ lesson-40 - 45/lesson40to45.js
@@ -1,8 +1,21 @@
 //This is data attributes=== How we can use data attributes in JS and how to add and read elements.
-const buttons = document.querySelectorAll("button")
-buttons.forEach(button =>{
-    button.addEventListener("click", () =>{
-      const afterClick = (parseInt(button.dataset.clicks))
-      button.dataset.clicks = afterClick + 1;
-    })
-})
+// const buttons = document.querySelectorAll("button")
+// buttons.forEach(button =>{
+//     button.addEventListener("click", () =>{
+//       const afterClick = (parseInt(button.dataset.clicks))
+//       button.dataset.clicks = afterClick + 1;
+//     })
+// })
+ 
+// same like top This is my 2nd Practice for TUTORIAL--41
+// buttons.forEach(button =>{
+//     button.addEventListener("click", () =>{
+//         const myPractice = (parseInt(button.dataset.clicks))
+//         button.dataset.clicks = myPractice + 1
+//     })
+// })
+
+           // Practice End Of 41 
+
+
+
